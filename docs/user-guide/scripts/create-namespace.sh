720ddfb21a0b3b6ea530176e55a462baf02213b7
Sum Huynh <shuynh@redhat.com>
2026-09-09 15:29:43 -0400

    remove from main
    Signed-off-by: Sum Huynh <shuynh@redhat.com>

--- docs/user-guide/scripts/create-namespace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-. ./common-vars.sh
-
-oc new-project "$INFRA_NAMESPACE" 2>/dev/null || oc project "$INFRA_NAMESPACE"
-oc new-project "$TENANT_NAMESPACE" 2>/dev/null || oc project "$TENANT_NAMESPACE"
--- docs/user-guide/scripts/install-operator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-OP_NAMESPACE="${1:-redhat-ods-applications}"
-helm install dc-controller dc-controller/charts/ --namespace "$OP_NAMESPACE" --create-namespace --set controllerManager.image.pullPolicy=Always
